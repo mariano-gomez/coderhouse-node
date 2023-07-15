c2ed4b19009dfbc74b5fe1681b3f49a07b8162af
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2023-07-15 19:40:15 -0300

    Agregando handlebars y socket.io, configurando el server para utilizar handlebars (y preparando las bases para usar socket.io), y agregando una ruta estandar (no API) para ver el listado de productos en la home

--- Desafio.04/app.js
+++ Desafio.04/app.js
@@ -1,18 +1,32 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const handlebars = require('express-handlebars');
+const { Server } = require('socket.io');
 
 const app = express();
-const { apiRoutes, otherRoutes } = require('./routes');
+const server = http.createServer(app);
+const io = new Server(server);
 
+//  setting the `handlebars` template engine
+app.engine('handlebars', handlebars.engine());
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'handlebars');
+
+//  Some global configs
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/static', express.static(path.join(__dirname + '/public')))
 
+//  setting the routes
+const { apiRoutes, standardRoutes } = require('./routes');
 app.use('/api', apiRoutes);
 
 //  To send an error response for any URL not supported
-app.use('*', otherRoutes);
+app.use('*', standardRoutes);
 
 const port = 8080;
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Express Server listening at http://localhost:${port}`);
 });
--- Desafio.04/routes/standard.router.js
+++ Desafio.04/routes/standard.router.js
@@ -1,13 +1,21 @@
 const Router = require('express');
+const ProductManager = require('../managers/ProductManager');
+const productManager = new ProductManager('products.json');
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+    const products = await productManager.getAll();
+    res.render('home', {
+        products
+    });
+});
+
 //  This method is meant to provide a response for any other endpoint/url that doesn't exists
-//  TODO: check what happens if i remove the '*' parameter)
 router.all('*', (req, res) => {
     res.statusCode = 404;
     console.log(`An invalid url has been required`);
     res.send({ "Error": "The required URL doesn't exists" });
 });
 
-module.exports = router
+module.exports = router;
