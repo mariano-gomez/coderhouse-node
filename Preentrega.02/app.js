2761bce8e2ebf292fe028bd64cf88a7cf6e048b7
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2023-08-21 18:53:06 -0300

    Quitando middlewares que agregaban usuario y carrito al request, y limpiando codigo innecesario de app.js

--- Preentrega.02/app.js
+++ Preentrega.02/app.js
@@ -8,11 +8,6 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
-const attachUserToRequestMiddleware = require('./middlewares/attach.user.to.request.middleware');
-const attachCartToRequestMiddleware = require('./middlewares/attach.cart.to.request.middleware');
-
-//  TODO: temporal, until we add user management
-const userModel = require('./dao/models/user.model');
 const dependencyContainer = require('./dependency.injection');
 const mongoConnectionString = 'mongodb+srv://coderhose_app:OUQoVf5WZ54IoRKL@cluster0.u8oklk1.mongodb.net/desarrollo_ecommerce?retryWrites=true&w=majority';
 
@@ -47,19 +42,10 @@ app.use(session({
 }));
 
 const socketManagerFunction = require('./websockets');
-const {apiRoutes, standardRoutes} = require("./routes");
-const productModel = require("./dao/models/product.model");
 
 async function startServer() {
     await mongoose.connect(mongoConnectionString);
     console.log('DB CONNECTED');
-
-    //  once i connect to the DB, i fetch a user
-    //  Until we start working with users, we temporarily hardcode a mockup user
-    app.use(attachUserToRequestMiddleware);
-
-    //  This middleware is made to have the logged user's cart in the request (just the id, not the whole cart)
-    app.use(attachCartToRequestMiddleware);
 
 //  setting the routes
     const { apiRoutes, standardRoutes } = require('./routes');
