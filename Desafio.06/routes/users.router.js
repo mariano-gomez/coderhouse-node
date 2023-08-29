f7a8d7a330dfadb988e5429e5a760ecc76b89fd9
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2023-08-29 01:45:51 -0300

    Reemplazando el login local "a mano" por el la estrategia `passport-local`, junto con bastante refactorizacion asociada a este cambio

--- Desafio.06/routes/users.router.js
+++ Desafio.06/routes/users.router.js
@@ -14,147 +14,48 @@ const router = Router();
 router.get('/login', isNotAuth, (req, res) => {
     res.render('users/login');
 });
-
-router.post('/login', isNotAuth, async (req, res) => {
-    //  Authentication comes in the next step, we only check the user
-    const { email, password } = req.body;
-
-    try {
-        const user = await userManager.getByEmail(email);
-        const role = (email == 'adminCoder@coder.com' && password == 'adminCod3r123') ? 'admin' : 'usuario';
-        if (!user || !isValidPassword(password, user.password)) {
-            throw new Error(`El usuario no existe, o la contraseña es incorrecta`);
-        }
-
-        delete user.password;
-        
-        req.session.user = {
-            name: user.firstname,
-            id: user._id,
-            role,
-            ...user
-        };
-
-        req.session.cart = await cartManager.getByUser(user._id);
-
-        // setear la cookie de usuario
-        //  si por alguna razon no se guarda lo q tenemos mas arriba, tenemos q usar el sgte `save()` (cuando usamos FileStore)
-        req.session.save((err) => {
-            if (!err) {
-                res
-                    .cookie('user', req.session.user.firstname)
-                    .redirect('/')
-            } else {
-                throw new Error(err);
-            }
-        });
-
-    } catch (e) {
-        res.render('users/login', {
-            error: e.message
-        });
-    }
-});
-
 router.get('/signup', (req, res) => {
     res.render('users/signup');
 });
-
-router.post('/signup', async (req, res) => {
-    const user = req.body;
-
-    //  crear al usuario
-    try {
-
-        const existing = await userManager.getByEmail(user.email);
-
-        if (existing) {
-            res.render('users/signup', {
-                error: 'El email ya existe'
-            });
-            return;
-        }
-
-        const role = (user.email == 'adminCoder@coder.com' && user.password == 'adminCod3r123') ? 'admin' : 'usuario';
-        user.password = hashPassword(user.password);
-        const newUser = await userManager.create(user);
-
-        req.session.user = {
-            id: newUser._id,
-            name: newUser.firstname,
-            role,
-            ...newUser._doc
-        };
-
-        const newCart = await cartManager.create(newUser._id);
-        req.session.cart = await cartManager.getById(newCart._id);
-
-        req.session.save((err) => {
-            res.redirect('/');
-        });
-    } catch (e) {
-        return res.render('users/signup', {
-            error: 'Ocurrio un error. Intentalo mas tarde'
-        })
-    }
-});
-
 router.get('/profile', isAuth, (req, res) => {
     res.render('users/profile', {
-        user: req.session?.user,
-        cid: req.session?.cart._id,
-        ...req.session?.user
+        user: req.user,
+        cid: req.user?.cart?._id,
+        ...req.user
     });
 });
 
-router.get('/github', passport.authenticate('github'), (req, res) => {});
+const loginHandler = (req, res) => {
+    //  i don't set it in the `options` object, because i need to set the cookie
+    res.cookie('user', req.user.firstname).redirect('/profile');
+};
+
+router.post('/login', isNotAuth, passport.authenticate('local-login', {
+    failureRedirect: '/login',
+}), loginHandler);
+
+router.post('/signup', isNotAuth, passport.authenticate('local-signup', {
+    failureRedirect: '/signup',
+}), loginHandler);
+
+router.get('/github', passport.authenticate('github', {
+    failureRedirect: '/signup',
+}), (req, res) => {});
 
 router.get('/githubSessions',
     passport.authenticate('github', { failureRedirect: '/github/fail' }),
-    async (req, res) => {
-        const user = req.user;
-        req.session.user = {
-            id: user._id,
-            name: user.firstname,
-            role: (user.email == 'adminCoder@coder.com' && user.password == 'adminCod3r123') ? 'admin' : 'usuario',
-            email: user.email,
-            firstname: user.firstname,
-            lastname: user.lastname,
-        };
-        req.session.cart = await cartManager.getByUser(user._id);
-
-        req.session.save((err) => {
-            if (err) {
-                console.log(err);
-            }
-            res.redirect('/');
-        });
-    }
+    loginHandler
 );
 
-const logoutCallback = (req, res) => {
-    if (req.user) {
-        //  req.user is only being set by passport. Therefore, if req.user exists, it is because the user has logged
-        //  with github
-        req.logOut((err) => {
-            if (err) {
-                res.redirect('/back');
-            } else {
-                res.redirect('/login');
-            }
-        });
-    } else {
-        //  If the user is logged via standard, classic form
-        req.session.destroy((err) => {
-            if(err) {
-                return res.redirect('back');
-            }
-            res.clearCookie('user').redirect('/login');
-        });
-    }
-};
-
-router.get('/logout', isAuth, logoutCallback);
+router.get('/logout', isAuth, (req, res) => {
+    req.logOut((err) => {
+        if (err) {
+            res.redirect('/back');
+        } else {
+            res.redirect('/login');
+        }
+    });
+});
 
 router.get('/github/fail', (req, res) => {
     res.send('There has been an error. Try again later');
