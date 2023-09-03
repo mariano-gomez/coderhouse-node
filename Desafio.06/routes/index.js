ab0cc101159be76638d14cd3f8473890cff80da4
DESKTOP-AK750H5\Mariano <marianogomez@gmail.com>
2023-09-02 23:40:01 -0300

    Agregando endpoint para ver datos del usuario logueado (usando sessions)

--- Desafio.06/routes/index.js
+++ Desafio.06/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+
+const { SESSIONLESS } = require("../config/global.variables.config");
+const apiAuthWithSessionsRoutes = require('./api/sessions.router');
 const productsRoutes = require('./api/products.router');
 const cartsRoutes = require('./api/carts.router');
 const standardRoutes = require('./standard.router');
@@ -7,6 +10,11 @@ const authRoutes = require('./users.router');
 const router = Router();
 router.use('/products', productsRoutes);
 router.use('/carts', cartsRoutes);
+
+if (!SESSIONLESS) {
+    //  enables routes that depends on sessions
+    router.use('/sessions', apiAuthWithSessionsRoutes);
+}
 
 module.exports = {
     apiRoutes: router,
